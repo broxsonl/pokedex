7d56df1a843f129a0a2470470c6671c406db57b0
Lee Broxson <broxsonl@gmail.com>
2017-01-24 18:29:47 -0800

    Fixed The off by one error the prev butrton was causing prior.

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 var express = require('express'),
-  // requestProxy = require('express-request-proxy'),
   port = 3000,
   app = express();
 
 app.use(express.static('./'));
-
-// app.get('*', function(request, response) {
-//   console.log('New request:', request.url);
-//   response.sendFile('index.html', {root: '.'});
-// });
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
